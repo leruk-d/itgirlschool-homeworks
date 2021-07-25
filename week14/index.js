a8a6b6015432ff5a3843cc891ff524b7c7b924f8
leruk-d <lerka3003@mail.ru>
2021-07-25 21:47:06 +0300

    валидация чисел в поле

--- week14/index.js
+++ week14/index.js
@@ -1,33 +1,49 @@
+function isNumValid(num1){
+return!Number.isNaN(Number(num1));
+}
 
 function calcSum() {
     let num1= document.getElementById('num1').value;
     let num2= document.getElementById('num2').value;
-
+if(isNumValid(num1) &&isNumValid(num2)){
     result = +num1 + +num2;
     document.getElementById('result').innerHTML=result;
+} else{
+        alert("Only numbers");
+    }
 }
 
 function calcSub() {
     let num1= document.getElementById('num1').value;
     let num2= document.getElementById('num2').value;
+if(isNumValid(num1) &&isNumValid(num2)){
     result = +num1 - +num2;
-
     document.getElementById('result').innerHTML=result;
+} else{
+    alert("Only numbers");
 }
+}
+
 function calcMult() {
     let num1= document.getElementById('num1').value;
     let num2= document.getElementById('num2').value;
+if(isNumValid(num1) &&isNumValid(num2)){
     result = +num1 * +num2;
-
     document.getElementById('result').innerHTML=result;
+} else{
+    alert("Only numbers");
+}
 }
 
 function calcDiv() {
     let num1= document.getElementById('num1').value;
     let num2= document.getElementById('num2').value;
-    result = +num1 / +num2;
-
+if(isNumValid(num1) &&isNumValid(num2)){
+    result = +num1 / +num2; 
     document.getElementById('result').innerHTML=result;
+} else{
+    alert("Only numbers");
+}
 }
 
 function clickMe(){
