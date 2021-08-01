25dde6d791cfbb93abd037e22d27dd2be5389e3d
leruk-d <lerka3003@mail.ru>
2021-08-01 22:11:17 +0300

    правки по первой части дз

--- week15/1/index.js
+++ week15/1/index.js
@@ -1,12 +1,17 @@
 function isNumValid(num1){
 return!Number.isNaN(Number(num1));
 }
-
-function calcSum() {
+function enterNumber() {
     let num1= document.getElementById('num1').value;
     let num2= document.getElementById('num2').value;
+    return [num1, num2];
+}
+
+function calcSum() {
+    let [num1, num2] = enterNumber();
+    
 if(isNumValid(num1) &&isNumValid(num2)){
-    result = +num1 + +num2;
+    let result = +num1 + +num2;
     document.getElementById('result').innerHTML=result;
 }else {
         alert("Only numbers");
@@ -14,10 +19,10 @@ if(isNumValid(num1) &&isNumValid(num2)){
 }
 
 function calcSub() {
-    let num1= document.getElementById('num1').value;
-    let num2= document.getElementById('num2').value;
+    let [num1, num2] = enterNumber();
+
 if(isNumValid(num1) &&isNumValid(num2)){
-    result = +num1 - +num2;
+   let result = +num1 - +num2;
     document.getElementById('result').innerHTML=result;
 } else{
     alert("Only numbers");
@@ -25,10 +30,10 @@ if(isNumValid(num1) &&isNumValid(num2)){
 }
 
 function calcMult() {
-    let num1= document.getElementById('num1').value;
-    let num2= document.getElementById('num2').value;
+    let [num1, num2] = enterNumber();
+
 if(isNumValid(num1) &&isNumValid(num2)){
-    result = +num1 * +num2;
+    let result = +num1 * +num2;
     document.getElementById('result').innerHTML=result;
 } else{
     alert("Only numbers");
@@ -37,13 +42,13 @@ if(isNumValid(num1) &&isNumValid(num2)){
 
 
 function calcDiv() {
-    let num1= document.getElementById('num1').value;
-    let num2= document.getElementById('num2').value;
-    (num2>0)? result:("На ноль делить нельзя!");
+    let [num1, num2] = enterNumber();
 if(isNumValid(num1) &&isNumValid(num2)){
-    result = +num1 / +num2; 
+    let result = +num1 / +num2; 
     document.getElementById('result').innerHTML=result;
-    (num2==0)? alert("На ноль делить нельзя!"):result;
+}
+if(num2 == 0) {
+        alert("На ноль делить нельзя!") 
 } else {
     alert("Only numbers");
 }
