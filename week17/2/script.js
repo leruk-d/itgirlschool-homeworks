e97611765624af93bf25512beea90c403f0df63a
Валерия Дубчак <lerka3003@mail.ru>
2021-08-24 03:12:40 +0300

    2задание

---
+++ week17/2/script.js
@@ -0,0 +1,19 @@
+let addInputBtn = document.getElementById("btn");
+let talkContainer = document.querySelector(".talk-container");
+let comment = document.getElementById("comments");
+let words = ['XXX', 'viagra']
+
+function checkSpam(comments) {
+    return `<div>${comments}</div> <hr>`;
+}
+
+addInputBtn.addEventListener("click", () => {
+   let commentInput = document.getElementById("comments");
+
+    if (!commentInput.value) return;
+    let str=commentInput.value.replace(/(viagra|xxx)/gi, '***');
+    let newCommentAsString = checkSpam(str);
+    talkContainer.innerHTML = talkContainer.innerHTML + newCommentAsString;
+    commentInput.value = "";
+});
+
