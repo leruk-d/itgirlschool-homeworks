8ccf311e7c6dfca79778153cedeb0d851ff2d2e7
Валерия Дубчак <lerka3003@mail.ru>
2021-08-25 13:37:52 +0300

    правки по первому дз

--- week17/1/script.js
+++ week17/1/script.js
@@ -11,31 +11,11 @@ function checkInput() {
   let fatherName = newstr[2];
   console.log(fatherName);
 
-
+let inputs= document.getElementsByName('name');
 for (let i = 0; i < newstr.length; i++) 
 {
   let arrName= newstr[i][0].toUpperCase() + newstr[i].slice(1).toLowerCase(); 
   console.log(arrName);
-
-  document.getElementById("surname").innerHTML=arrName[0];
-  document.getElementById("name").innerHTML=arrName[1];
-  document.getElementById("patronymic").innerHTML=arrName[2];
+  inputs[i].value=arrName;
 };
-// let inputFirstName = document.querySelector("surname");
-
-// let newFirstName= firstName[0].toUpperCase() + firstName.slice(1).toLowerCase();
-// console.log(newFirstName);
-// let newLastName= lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
-// console.log(newLastName);
-// let newFatherName= fatherName[0].toUpperCase() + fatherName.slice(1).toLowerCase();
-// console.log(newFatherName);
-
-// let firstNameContainer = document.querySelector(".surname");
-// let firstLastName = document.querySelector(".name");
-// let firstFatherName = document.querySelector(".patronymic");
-
-
-  // document.getElementById("surname").innerHTML=newFirstName;
-  // document.getElementById("name").innerHTML=newLastName;
-  // document.getElementById("patronymic").newFatherName;
 }
