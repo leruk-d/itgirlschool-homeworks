78a52a39bca2e33a97822ad39074e94758a53a04
Валерия Дубчак <lerka3003@mail.ru>
2021-08-31 15:40:38 +0300

    исправила 4 часть

--- week17/4/script.js
+++ week17/4/script.js
@@ -1,43 +1,92 @@
+// function generationNumb() {
+//     let arrNumbers = [];
+//     console.log(arrNumbers);
+//     for (let i = 0; i < 10; i++) {
+//         let numberRandom = Math.floor(Math.random() * 21) - 10;
+//         console.log(numberRandom);
+//         arrNumbers.push(numberRandom); 
+//     };
+
+// }
+
+
+// function elementMax(arrNumbers) {
+//     let max = Math.max.apply(null, arrNumbers);
+//     console.log(max);
+// };
+
+// function elementMin(arrNumbers) {
+//     let min = Math.min.apply(null, arrNumbers);
+//     console.log(min);
+// };
+
+// //сумма через функцию
+// function elementsSum(arrNumbers) {
+//     let sum = 0;
+//     for (let i = 0; i < arrNumbers.length; i++) {
+//         sum += arrNumbers[i];
+//     }
+//     console.log(sum);
+// }
+// elementsSum(arrNumbers);
+
+// // среднее арифметическое
+// function elementsAverage(arrNumbers){
+//     let sum = 0;
+//     for (let i = 0; i < arrNumbers.length; i++) {
+//         sum += arrNumbers[i];
+//     }
+//     let average=sum/arrNumbers.length;
+//     console.log(average);
+// };
+
+// function elementsMult(arrNumbers){
+//     let mult=arrNumbers.reduce((a,b)=>a*b);
+//     console.log(mult);
+// }
+
+let numbers=document.querySelector('#numbers');
+let btn = document.querySelector('#btn');
+let max = document.querySelector('#max');
+let min = document.querySelector('#min');
+let average = document.querySelector('#average');
+let sum = document.querySelector('#sum');
+let mult = document.querySelector('#mult');
+
 function generationNumb() {
-    let numberRandom = Math.floor(Math.random() * 21) - 10;
-    console.log(numberRandom);
-    arrNumbers.push(numberRandom);
-}
-let arrNumbers = [];
-console.log(arrNumbers);
+    let arrNumbers = [];
+    console.log(arrNumbers);
+    for (let i = 0; i < 10; i++) {
+        let numberRandom = Math.floor(Math.random() * 21) - 10;
+        arrNumbers.push(numberRandom); 
+    };
+numbers.innerHTML = arrNumbers;
 
-function elementMax(arrNumbers) {
-    let max = Math.max.apply(null, arrNumbers);
-    console.log(max);
-};
 
-function elementMin(arrNumbers) {
-    let min = Math.min.apply(null, arrNumbers);
-    console.log(min);
-};
+    let maxNumber = Math.max.apply(null, arrNumbers);
+    max.innerHTML ='Максимальное:'+" "+ maxNumber;
 
-//сумма через функцию
-function elementsSum(arrNumbers) {
-    let sum = 0;
+
+    let minNumber = Math.min.apply(null, arrNumbers);
+min.innerHTML='Минимальное:'+" "+minNumber;
+
+
+    let sumNumbers = 0;
     for (let i = 0; i < arrNumbers.length; i++) {
-        sum += arrNumbers[i];
+        sumNumbers += arrNumbers[i];
     }
-    console.log(sum);
-}
-elementsSum(arrNumbers);
+sum.innerHTML ='Сумма:'+" "+sumNumbers;
 
-// среднее арифметическое
-function elementsAverage(arrNumbers){
-    let sum = 0;
+
+
     for (let i = 0; i < arrNumbers.length; i++) {
-        sum += arrNumbers[i];
+        sumNumbers += arrNumbers[i];
     }
-    let average=sum/arrNumbers.length;
-    console.log(average);
-};
+    let averageNumbers=sumNumbers/arrNumbers.length;
+    average.innerHTML = 'Среднее арифметическое:'+" "+averageNumbers;
 
-function elementsMult(arrNumbers){
-    let mult=arrNumbers.reduce((a,b)=>a*b);
-    console.log(mult);
+
+    let multNumbers=arrNumbers.reduce((a,b)=>a*b);
+mult.innerHTML ='Произведение чисел:'+" "+ multNumbers;
+
 }
-
