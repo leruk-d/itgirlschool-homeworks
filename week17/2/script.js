fd3a5d2d252b11708c1b4149f726bd598020b9b1
Валерия Дубчак <lerka3003@mail.ru>
2021-08-31 12:31:59 +0300

    правки по 2 части

--- week17/2/script.js
+++ week17/2/script.js
@@ -4,16 +4,26 @@ let comment = document.getElementById("comments");
 
 
 function checkSpam(comments) {
+    addInputBtn.addEventListener("click", () => {
+        let commentInput = document.getElementById("comments");
+     
+         if (!commentInput.value) return;
+         let str=commentInput.value.replace(/(viagra|виагра|xxx)/gi, '***');
+         let newCommentAsString = checkSpam(str);
+         talkContainer.innerHTML = talkContainer.innerHTML + newCommentAsString;
+         commentInput.value = "";
+     });
     return `<div>${comments}</div> <hr>`;
+    
 }
 
-addInputBtn.addEventListener("click", () => {
-   let commentInput = document.getElementById("comments");
+// addInputBtn.addEventListener("click", () => {
+//    let commentInput = document.getElementById("comments");
 
-    if (!commentInput.value) return;
-    let str=commentInput.value.replace(/(viagra|xxx)/gi, '***');
-    let newCommentAsString = checkSpam(str);
-    talkContainer.innerHTML = talkContainer.innerHTML + newCommentAsString;
-    commentInput.value = "";
-});
+//     if (!commentInput.value) return;
+//     let str=commentInput.value.replace(/(viagra|виагра|xxx)/gi, '***');
+//     let newCommentAsString = checkSpam(str);
+//     talkContainer.innerHTML = talkContainer.innerHTML + newCommentAsString;
+//     commentInput.value = "";
+// });
 
