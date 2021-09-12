5dbac43be3ad841c327b04ae8310a692558a531b
leruk-d <lerka3003@mail.ru>
2021-09-12 23:37:54 +0300

    2 задание

--- week19/2/script.js
+++ week19/2/script.js
@@ -1,59 +1,46 @@
-function check(){
-    class Cat{
-        construktor(petName,name,tel,petBreed,feed,sexCat){
-            this.petName=petName;
-            this.name=name;
-            this.tel=tel;
-            this.petBreed=petBreed;
-            this.feed=feed;
-            this.sexCat=sexCat;
-        }
-    }
-    
-    let petName1=document.getElementById('name').value;
-    let name1=document.getElementById('name1').value;
-    let tel=document.getElementById('phone').value;
-    let petBreed=document.getElementById('breed').value;
-  
-  
-    
-    let cat1=new Cat(petName1,name1);
-    console.log()
-}
-class Cat{
-    construktor(petName,name,tel,petBreed){
-        this.petName=petName;
-        this.name=name;
-        this.tel=tel;
-        this.petBreed=petBreed;
-        this.feed=feed;
-        this.sexCat=this.sexCat;
-    }
-}
-button.onclick=function Cat () {
-    let petName1=document.getElementById('name').value;
-    let name1=document.getElementById('name1').value;
-    let tel1=document.getElementById('phone').value;
-    let breed1=document.getElementById('breed').value;
-    let pet1=document.getElementById('pet').value;  
-}
-Cat ();
-console.log(Cat)
+ class Cat {
+     construktor(petName, name, tel, petBreed, feed, sexCat) {
+         this.petName = petName;
+         this.name = name;
+         this.tel = tel;
+         this.petBreed = petBreed;
+         this.feed = feed;
+         this.sexCat = sexCat;
+     }
+ }
+ function newCat() {
+
+     let petName1 = document.getElementById('name').value;
+     let name1 = document.getElementById('name1').value;
+     let tel1 = document.getElementById('phone').value;
+     let petBreed = document.querySelector('option[class="breed"]:checked').value;
+ let feed=getCheckBox() ;
+     let sexCat = checkSex();
+
+let cat1= new Cat(petName1, name1, tel1, petBreed, feed, sexCat);
+console.log(cat1);
+ }
 
 
-let cat1=new Cat(Cat ());
-console.log()
-
-function getCheckBox(){
-    let chackboxes=document.getElementsByClassName('pet');
-    let arr=[];
-    console.log(arr);
-    for( let i =0; i < arr.length; i++){
-        if(chackboxes[i].checked){
+ function getCheckBox() {
+     let chackboxes = document.getElementsByClassName('pet');
+     let arr = [];
+     for (let i = 0; i < arr.length; i++) {
+         if (chackboxes[i].checked) {
             arr.push(chackboxes[i].value);
-            let pet =hackboxes[i].value;
-        }
-    }
-    return arr;
-}  
-console.log (getCheckBox())
+         }
+     }
+ }
+
+
+ function checkSex() {
+     let radioSex = document.getElementsByClassName('sex');
+     for (i = 0; i < radioSex.length; i++) {
+         if (radioSex[i].checked) {
+             break;
+         }
+     }
+     return radioSex[i].value;
+ }
+
+// Создает объект cat1, но без свойств?!
