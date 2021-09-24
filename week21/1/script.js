635a4b049fb57cccb70b466103d9c2368b93350a
leruk-d <lerka3003@mail.ru>
2021-09-24 22:41:12 +0300

    1 задание и правки по 3

---
+++ week21/1/script.js
@@ -0,0 +1,17 @@
+
+function search(){
+    let searchValue=document.getElementById("search").value;
+    let api_key="2crkCOKP6fb7R7sRcFkAwG82ushIijkU";
+    document.getElementById("container").innerHTML = "";
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchValue}&limit=5&offset=0&rating=g&lang=en`)
+    .then(response=>response.json())
+    .then(json => {
+        console.log(json);
+        for (let i = 0; i < json.data.length ; i++) {
+            let url = json.data[i].images.original.url;
+            document.getElementById("container").innerHTML += `<img src="${url}" alt="gif">`;
+        }
+    })
+    .catch(err => console.log(err));
+}
