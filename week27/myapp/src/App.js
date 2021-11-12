984cda214c07e36e89c4c15de868769b5dc314d5
Валерия Дубчак <lerka3003@mail.ru>
2021-11-13 00:46:58 +0300

    первая часть практики(карточки с тарифами)

--- week27/myapp/src/App.js
+++ week27/myapp/src/App.js
@@ -2,10 +2,10 @@ import Card from './components/Card'
 import './App.css';
 
 const cards=[
-  {id:'1', rate:'300', price:'300',  speed:'10'},
-  {id:'2', rate:'450', price:'450',  speed:'50'},
-  {id:'3', rate:'500', price:'500',  speed:'100', isSelected: true},
-  {id:'4', rate:'1000', price:'1000',  speed:'200'}
+  {id:'1', price:'300',  speed:'10'},
+  {id:'2', price:'450',  speed:'50'},
+  {id:'3', price:'500',  speed:'100', isSelected: true},
+  {id:'4', price:'1000',  speed:'200'}
 ]
 
 function App() {
--- week27/myapp/src/components/Card.jsx
+++ week27/myapp/src/components/Card.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import './Card.css';
+import './Card.scss';
 
 function Card(props) {
     return (
-        <div className={`card ${isSelected ? 'selected' : ''}`}>
-            <div className='card-header'>Безлимитный {props.rate}</div>
+        <div className={`card ${props.isSelected ? 'selected' : ''}`}>
+            <div className={`card-header${props.price}`}>Безлимитный {props.price}</div>
             <div className='card-body'>
-                <div className='card-price'>
-                    <div className='card-text1'>руб</div>
-                    <div className='price-value'>{props.price}</div>
-                    <div className='card-text2'>/мес</div>
-                </div>
-                <div className='card-speed'>до {props.speed} Мбит/сек</div>
-                <div className='card-footer'>Объем включенного трафика не граничен</div>
+                <div className={`card-price${props.price}`}>
+                <div className='card-text1'>руб</div>
+                <div className='card-value'>{props.price}</div>
+                <div className='card-text2'>/мес</div>
             </div>
+            <div className='card-speed'>до {props.speed} Мбит/сек</div>
+            <div className='card-footer'>Объем включенного трафика не граничен</div>
         </div>
+        </div >
     )
 }
 
