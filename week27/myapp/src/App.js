0414dcc11f1e8ff76990c64a583f9858a48f8931
Валерия Дубчак <lerka3003@mail.ru>
2021-11-13 18:25:06 +0300

    +

--- week27/myapp/src/App.js
+++ week27/myapp/src/App.js
@@ -1,20 +1,24 @@
-import Card from './components/Card'
-import './App.css';
+import Card from "./components/Card";
+import "./App.css";
 
-const cards=[
-  {id:'1', price:'300',  speed:'10'},
-  {id:'2', price:'450',  speed:'50'},
-  {id:'3', price:'500',  speed:'100', isSelected: true},
-  {id:'4', price:'1000',  speed:'200'}
-]
+const cards = [
+  { id: "1", price: "300", speed: "10" },
+  { id: "2", price: "450", speed: "50" },
+  { id: "3", price: "500", speed: "100", isSelected: true },
+  { id: "4", price: "1000", speed: "200" },
+];
 
 function App() {
   return (
     <div className="App">
-      
       {cards.map((card, i) => (
-          <Card price={card.price} key={card.id} speed={card.speed} isSelected={card.isSelected} />
-        ))}
+        <Card
+          price={card.price}
+          key={card.id}
+          speed={card.speed}
+          isSelected={card.isSelected}
+        />
+      ))}
     </div>
   );
 }
