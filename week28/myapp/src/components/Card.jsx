8d9f62d161d7f018f211e08ea576b78f66bdc5d2
Валерия Дубчак <lerka3003@mail.ru>
2021-11-19 22:58:16 +0300

    +

---
+++ week28/myapp/src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Card.scss";
+
+function Card(props) {
+  return (
+    <div className={`card ${props.isSelected ? "selected" : ""}`}>
+      <div className={`card-header${props.price}`}>
+        Безлимитный {props.price}
+      </div>
+      <div className="card-body">
+        <div className={`card-price${props.price}`}>
+          <div className="card-text1">руб</div>
+          <div className="card-value">{props.price}</div>
+          <div className="card-text2">/мес</div>
+        </div>
+        <div className="card-speed">до {props.speed} Мбит/сек</div>
+        <div className="card-footer">Объем включенного трафика не граничен</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
